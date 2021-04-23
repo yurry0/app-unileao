d0afef389c098367b6ada2af1c774eec5fa61bf1
Yuri Martins <yurimartinsr@gmail.com>
2021-04-23 19:06:55 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+
+
+</head>
+
+<body>
+
+<?php echo "Aula - Gestão de Configuração"?>
+
+
+
+
+</body>
+</html>
